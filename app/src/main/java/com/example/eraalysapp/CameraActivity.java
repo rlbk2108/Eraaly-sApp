7520f26179233af311b4f099c46516afc86cb70f
Eraaly <kasymbekov_e@iuca.kg>
2022-04-16 14:26:20 +0600

    Bug fixes

---
+++ app/src/main/java/com/example/eraalysapp/CameraActivity.java
@@ -0,0 +1,162 @@
+package com.example.eraalysapp;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.extensions.HdrImageCaptureExtender;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.provider.MediaStore;
+import android.util.Size;
+import android.view.OrientationEventListener;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class CameraActivity extends AppCompatActivity {
+    private static final int PERMISSION_REQUEST_CAMERA = 83854;
+
+    private ImageView preview, imageView2;
+
+
+    ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+
+    YUVtoRGB translator = new YUVtoRGB();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        preview = findViewById(R.id.preview);
+        imageView2 = findViewById(R.id.imageView2);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA},
+                    PERMISSION_REQUEST_CAMERA);
+        } else {
+            initializeCamera();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CAMERA && grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            initializeCamera();
+        }
+    }
+
+    private void initializeCamera() {
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+
+                    //Preview preview = new Preview.Builder().build();
+
+                    //ImageCapture imageCapture = new ImageCapture.Builder().build();
+
+                    ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
+                            .setTargetResolution(new Size(1366, 768))
+                            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                            .build();
+
+                    CameraSelector cameraSelector = new CameraSelector.Builder()
+                            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                            .build();
+
+                    imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(CameraActivity.this),
+                            new ImageAnalysis.Analyzer() {
+                                @Override
+                                public void analyze(@NonNull ImageProxy image) {
+                                    Image img = image.getImage();
+                                    Bitmap bitmap = translator.translateYUV(img, CameraActivity.this);
+
+                                    int size = bitmap.getWidth() * bitmap.getHeight();
+                                    int[] pixels = new int[size];
+                                    bitmap.getPixels(pixels, 0, bitmap.getWidth(), 0, 0,
+                                            bitmap.getWidth(), bitmap.getHeight());
+
+                                    bitmap.setPixels(pixels, 0, bitmap.getWidth(), 0, 0,
+                                            bitmap.getWidth(), bitmap.getHeight());
+
+                                    preview.setRotation(image.getImageInfo().getRotationDegrees());
+                                    preview.setImageBitmap(bitmap);
+                                    image.close();
+                                }
+                            });
+
+                    cameraProvider.bindToLifecycle(CameraActivity.this, cameraSelector, imageAnalysis);
+
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    public void takePhoto(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        ActivityResultLauncher<Intent> startActivityForResult = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == AppCompatActivity.RESULT_OK) {
+                        Intent data = result.getData();
+                    }
+                }
+        );
+        startActivityForResult.launch(intent);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100){
+            assert data != null;
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            imageView2.setImageBitmap(bitmap);
+        }
+    }
+
+}
--- app/src/main/java/com/example/eraalysapp/MainActivity.java
+++ app/src/main/java/com/example/eraalysapp/MainActivity.java
@@ -1,72 +1,128 @@
 package com.example.eraalysapp;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Camera;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.util.Size;
+import android.view.OrientationEventListener;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.core.VideoCapture;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.common.util.concurrent.ListenableFuture;
 
-    Button myButton;
-    CharSequence buttonText;
-    ImageButton myImageButton;
+import java.io.File;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 
-    private EditText mName;
-    private ImageButton mGo;
+public class MainActivity extends AppCompatActivity{
+    private SharedPreferences shp;
+    private EditText editSave;
+    private final String save_key = "save_key";
+    public static final String MyPREFERENCES = "MyPre";
+    Bitmap bitmap;
+    Camera camera;
+    private static final String[] CAMERA_PERMISSION = new String[]{Manifest.permission.CAMERA};
+    private static final int CAMERA_REQUEST_CODE = 10;
+    private CameraManager mCameraManager = null;
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mName=findViewById(R.id.editTextTextPersonName);
-        mGo=findViewById(R.id.imageButton3);
+        init();
     }
 
-    
-
-
-    public void onMyButtonClick(View view) {
-        myButton = findViewById(R.id.button1); // получаем нужную кнопку по ID
-        myButton.setText("Kasymbekov"); // изменяем текст кнопки
-
-        // выводим сообщение
-        Toast.makeText(this, "I changed it to my last name!", Toast.LENGTH_LONG).show();
+    private boolean hasCameraPermission() {
+        return ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED;
     }
 
-
-    public void onButtonClick(View view) {
-        myImageButton = findViewById(R.id.imageButton1); // получаем нужную кнопку по ID
-        buttonText = myButton.getText(); // получаем текст кнопки
-        if (buttonText == "Kasymbekov") {
-            myButton.setText("Eraaly");
-            Toast.makeText(this, "Now the text is my first name.", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, "You need to click other button and try again.", Toast.LENGTH_SHORT).show();
-        }
-
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(
+                this,
+                CAMERA_PERMISSION,
+                CAMERA_REQUEST_CODE
+        );
     }
 
-    public void onClick(View view) {
+    private void enableCamera() {
+        Intent intent = new Intent(this, CameraActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickSave(View view) {
+        SharedPreferences.Editor edit = shp.edit();
+        edit.putString(save_key, editSave.getText().toString());
+        edit.apply();
+        Toast.makeText(MainActivity.this, "Text Saved", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickGet(View view) {
+        editSave.setText(shp.getString(save_key, "empty"));
+        Toast.makeText(MainActivity.this, "Text Loaded", Toast.LENGTH_SHORT).show();
+    }
+
+    private void init() {
+        shp = getSharedPreferences("dataRow", MODE_PRIVATE);
+        editSave = findViewById(R.id.editTextTextPersonName);
+        editSave.setText(shp.getString(save_key, "Data is empty"));
+    }
+
+    public void openSecondActivity(View view) {
         Intent intent = new Intent(this, SecondActivity.class);
         startActivity(intent);
     }
 
-    public void Send(View view) {
-        switch (view.getId()) {
-            case R.id.imageButton3:
-                Intent intent = new Intent(this, SecondActivity.class);
-                //Создаем данные для передачи:
-                intent.putExtra("name", mName.getText().toString());
-                //Запускаем переход:
-                startActivity(intent);
-                break;
-            default: break;
+    public void onClickDelete(View view) {
+        SharedPreferences.Editor edit = shp.edit();
+        edit.clear();
+        edit.apply();
+        Toast.makeText(MainActivity.this, "Text Deleted", Toast.LENGTH_SHORT).show();
+    }
+
+    public void openCamera(View view) {
+        if (hasCameraPermission()) {
+            enableCamera();
+        } else {
+            requestPermission();
         }
     }
 }
+
+
